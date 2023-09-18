b1063d8f7470258c8f2a3c8f9945c7291d7f72d7
Wesley Taylor <jamsterwes@gmail.com>
2023-09-17 20:36:34 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  resources :books
-  root 'books#index'
+  # Old routing
+  # devise_for :admins
+  # resources :books
+  # root 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # -=-=-=-=-
+  # New routing
+  root 'dashboards#show'
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+  devise_scope :admin do
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end
 end
