c307decb23c813c1761c523adecd771a8620eb24
Wes Taylor <jamsterwes@gmail.com>
2023-09-22 12:58:01 -0500

    added login email tests, updated User to have required fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_22_170823) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_175005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "full_name"
-    t.string "uid"
-    t.string "avatar_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -42,6 +32,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_22_170823) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_admin"
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
