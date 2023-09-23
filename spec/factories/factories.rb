3f7ba4219e130868ccf2bd844139eb6204cfbb24
Wesley Taylor <jamsterwes@gmail.com>
2023-09-22 19:41:41 -0500

    added some RSpec login_user/login_admin macros

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -6,4 +6,11 @@ FactoryBot.define do
         avatar_url { '/' }
         is_admin { false }
     end
+    factory :admin_user do
+        email { 'admin@gmail.com' }
+        uid { '00000' }
+        full_name { 'Admin User' }
+        avatar_url { '/' }
+        is_admin { true }
+    end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,7 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+
+  config.extend ControllerMacros, type: :controller
+  config.extend ControllerMacros, type: :view
 end
