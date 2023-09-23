67d917c0c950f16f287263149ffb09911dcc686f
Wesley Taylor <jamsterwes@gmail.com>
2023-09-22 22:54:36 -0500

    trying to redirect user, not going well

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -19,11 +19,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    if resource_or_scope
-      stored_location_for(resource_or_scope)
-    else
-      if current_user.has_profile?
-    end
+    stored_location_for(resource_or_scope) || root_path
   end
 
   private
