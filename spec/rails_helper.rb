1ab6a7f5c74ea408e47f98d29a161044c0086e9c
Wesley Taylor <jamsterwes@gmail.com>
2023-09-22 20:54:46 -0500

    added integration test for User Bar

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,7 +63,9 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :system
 
   config.extend ControllerMacros, type: :controller
+  config.extend ControllerMacros, type: :system
   config.extend ControllerMacros, type: :view
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,12 +1,12 @@
 module ControllerMacros
   def login_admin
     before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      @request.env["devise.mapping"] = Devise.mappings[:user]
       sign_in FactoryBot.create(:admin)
     end
   end
   
-  def login_admin
+  def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       user = FactoryBot.create(:user)
