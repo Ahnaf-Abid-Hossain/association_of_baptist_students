cb59f55a8618254cb838757fa164567ad3fec006
Mario Morelos <mmorelos2@tamu.edu>
2023-09-24 14:28:09 -0500

    Prayer request mvp created

--- app/controllers/prayer_requests_controller.rb
+++ app/controllers/prayer_requests_controller.rb
@@ -3,11 +3,16 @@ class PrayerRequestsController < ApplicationController
 
   # GET /prayer_requests or /prayer_requests.json
   def index
-    @prayer_requests = PrayerRequest.all
+    if current_user.is_admin?
+      @prayer_requests = PrayerRequest.all
+    else
+      @prayer_requests = current_user.alumni.prayer_requests
+    end
   end
 
   # GET /prayer_requests/1 or /prayer_requests/1.json
   def show
+    authorize_prayer_request(@prayer_request)
   end
 
   # GET /prayer_requests/new
@@ -17,11 +22,15 @@ class PrayerRequestsController < ApplicationController
 
   # GET /prayer_requests/1/edit
   def edit
+    authorize_prayer_request(@prayer_request)
   end
 
   # POST /prayer_requests or /prayer_requests.json
   def create
-    @prayer_request = PrayerRequest.new(prayer_request_params)
+    @prayer_request = current_user.alumni.prayer_requests.build(prayer_request_params)
+    if !current_user.is_admin?
+      @prayer_request.status = "not_read"
+    end
 
     respond_to do |format|
       if @prayer_request.save
@@ -36,6 +45,7 @@ class PrayerRequestsController < ApplicationController
 
   # PATCH/PUT /prayer_requests/1 or /prayer_requests/1.json
   def update
+    authorize_prayer_request(@prayer_request)
     respond_to do |format|
       if @prayer_request.update(prayer_request_params)
         format.html { redirect_to prayer_request_url(@prayer_request), notice: "Prayer request was successfully updated." }
@@ -49,6 +59,7 @@ class PrayerRequestsController < ApplicationController
 
   # DELETE /prayer_requests/1 or /prayer_requests/1.json
   def destroy
+    authorize_prayer_request(@prayer_request)
     @prayer_request.destroy
 
     respond_to do |format|
@@ -65,6 +76,18 @@ class PrayerRequestsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def prayer_request_params
-      params.require(:prayer_request).permit(:request, :status, :alumni_id)
+      if current_user.is_admin?
+        params.require(:prayer_request).permit(:request, :status)
+      else
+        params.require(:prayer_request).permit(:request)
+      end
+    end
+
+    def authorize_prayer_request(prayer_request)
+      return if current_user.is_admin?
+
+      unless prayer_request.alumni.user_id == current_user.id
+        redirect_to prayer_requests_path, alert: 'You are not authorized to perform this action.'
+      end
     end
 end
--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -1,3 +1,4 @@
 class Alumni < ApplicationRecord
   belongs_to :user
+  has_many :prayer_requests
 end
