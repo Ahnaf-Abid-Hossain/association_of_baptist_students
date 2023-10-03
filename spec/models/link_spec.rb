ddee522cd6165b9fc5b44f3bf48b8561668fc729
Wesley Taylor <jamsterwes@gmail.com>
2023-10-03 13:29:50 -0500

    wrote Model tests for Link

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe(Link) do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid when filled out' do
+    link = described_class.create!(url: 'test.com', label: 'Test', order: 0)
+    expect(link).to(be_valid)
+  end
+
+  it 'is not valid with a negative order' do
+    link = described_class.create!(url: 'test.com', label: 'Test', order: -2)
+    expect(link).not_to(be_valid)
+  end
+
+  it 'is not valid without a label' do
+    link = described_class.create!(url: 'test.com', order: -2)
+    expect(link).not_to(be_valid)
+  end
+
+  it 'is not valid without an order' do
+    link = described_class.create!(url: 'test.com', label: 'Test')
+    expect(link).not_to(be_valid)
+  end
+
+  it 'is not valid without a url' do
+    link = described_class.create!(label: 'Test', order: 2)
+    expect(link).not_to(be_valid)
+  end
+
+  it 'is not valid with conflicting orders' do
+    described_class.create!(label: 'Test', order: 2)
+    link2 = described_class.create!(label: 'Test 2', order: 2)
+    expect(link2).not_to(be_valid)
+  end
 end
