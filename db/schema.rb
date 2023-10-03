7c175a1c8118e70e8e8b89d5092200624ccae78d
Wesley Taylor <jamsterwes@gmail.com>
2023-10-03 13:13:44 -0500

    fixed title of site

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_021428) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_172256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "full_name"
-    t.string "uid"
-    t.string "avatar_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-  end
 
   create_table "alumnis", force: :cascade do |t|
     t.string "alum_first_name"
@@ -48,6 +38,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_021428) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "links", force: :cascade do |t|
+    t.string "label"
+    t.string "url"
+    t.integer "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "meeting_notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -68,14 +66,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_021428) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "is_admin"
     t.string "full_name"
     t.string "uid"
     t.string "avatar_url"
-    t.boolean "is_admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "meeting_notes", "alumnis"
