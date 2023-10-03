354b7b38a86419978660f46808d1217a10d90b09
Wesley Taylor <jamsterwes@gmail.com>
2023-10-03 14:07:07 -0500

    added links to navbar, moved navbar out of each individual page, now in application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_links
+
+  protected
+  def set_links
+    @links = Link.order(order: :asc)
+  end
 end
