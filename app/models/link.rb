011221517c1276bb2e3f48c8477152cbfa547530
Wesley Taylor <jamsterwes@gmail.com>
2023-10-03 14:29:21 -0500

    removed uniqueness constraint on order, fixed link_spec

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < ApplicationRecord
   validates :url, presence: true
   validates :label, presence: true
-  validates :order, uniqueness: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :order, numericality: { greater_than_or_equal_to: 0 }
 end
