293ecfb277fa61d9ad9849bb942a2e8ced4a4b13
Jacob Parker <parkerjacob37@gmail.com>
2023-10-07 15:30:31 +0000

    Adjusted some spec files, migrations should be fixed

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe usersController, type: :routing do
+RSpec.describe UsersController, type: :routing do
   describe "routing" do
     it "routes to #index" do
       expect(get: "/users").to route_to("users#index")
