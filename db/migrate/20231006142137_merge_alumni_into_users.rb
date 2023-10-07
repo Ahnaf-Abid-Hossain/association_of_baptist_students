0a67183971db17ad4c00c61104e645523b4591f1
Jacob Parker <parkerjacob37@gmail.com>
2023-10-07 13:46:29 +0000

    Eighth set of migration changes

--- db/migrate/20231006142137_merge_alumni_into_users.rb
+++ db/migrate/20231006142137_merge_alumni_into_users.rb
@@ -17,13 +17,13 @@ class MergeAlumniIntoUsers < ActiveRecord::Migration[7.0]
       user.update(user_first_name: alumni.alum_first_name, user_last_name: alumni.alum_last_name, user_contact_email: alumni.alum_email, user_ph_num: alumni.alum_ph_num, user_class_year: alumni.alum_class_year, user_job_field: alumni.alum_job_field, user_location: alumni.alum_location, user_status: alumni.alum_status, user_major: alumni.alum_major)
     end
 
-    rename_foreign_keys
+    # rename_foreign_keys
 
     # drop_table :alumnis
   end
 
-  def rename_foreign_keys
-    rename_column :meeting_notes, :alumnis, :users, column: "id"
-    rename_column :prayer_requests, :alumnis, :users, column: "id"
-  end
+  # def rename_foreign_keys
+    # rename_column :meeting_notes, :alumnis, :users, column: "id"
+    # rename_column :prayer_requests, :alumnis, :users, column: "id"
+  # end
 end
