68605f53be74676835dd6093918e6dcea8ea4248
Jacob Parker <parkerjacob37@gmail.com>
2023-10-07 06:35:56 +0000

    Sixth set of migration changes

--- db/migrate/20231006142137_merge_alumni_into_users.rb
+++ db/migrate/20231006142137_merge_alumni_into_users.rb
@@ -1,4 +1,4 @@
-class MergeuserIntoUsers < ActiveRecord::Migration[7.0]
+class MergeAlumniIntoUsers < ActiveRecord::Migration[7.0]
   def change
     # adding new columns
     add_column :users, :user_first_name, :string
@@ -11,19 +11,19 @@ class MergeuserIntoUsers < ActiveRecord::Migration[7.0]
     add_column :users, :user_status, :string # need this one?
     add_column :users, :user_major, :string
 
-    # migrate data from user to User
-    user.all.each do |user|
-      user = User.find_by(email:user.user_email)
-      user.update(user_first_name: user.user_first_name, user_last_name: user.user_last_name, user_contact_email: user.user_email, user_ph_num: user.user_ph_num, user_class_year: user.user_class_year, user_job_field: user.user_job_field, user_location: user.user_location, user_status: user.user_status, user_major: user.user_major)
+    # migrate data from Alumni to User
+    Alumni.all.each do |alumni|
+      user = User.find_by(email:alumni.alum_email)
+      user.update(user_first_name: alumni.alum_first_name, user_last_name: alumni.alum_last_name, user_contact_email: alumni.alum_email, user_ph_num: alumni.alum_ph_num, user_class_year: alumni.alum_class_year, user_job_field: alumni.alum_job_field, user_location: alumni.alum_location, user_status: alumni.alum_status, user_major: alumni.alum_major)
     end
 
     rename_foreign_keys
 
-    # drop_table :users
+    # drop_table :alumnis
   end
 
   def rename_foreign_keys
-    rename_column :meeting_notes, :users, :users, column: "id"
-    rename_column :prayer_requests, :users, :users, column: "id"
+    rename_column :meeting_notes, :alumnis, :users, column: "id"
+    rename_column :prayer_requests, :alumnis, :users, column: "id"
   end
 end
