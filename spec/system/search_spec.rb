8ff1dee0cd5ce2e68e496f821cf33dff945c56c5
Jacob Parker <parkerjacob37@gmail.com>
2023-10-12 21:50:28 +0000

    Contains all advanced search tests

--- spec/system/search_spec.rb
+++ spec/system/search_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe('Advanced Search') do
     driven_by(:rack_test)
   end
 
-  it 'displays the name of the logged in user' do
+  # First Name search test
+  it 'displays the name of the logged in user, when given first name' do
     # Sign in
     user = FactoryBot.create(:user)
     sign_in user
@@ -15,6 +16,71 @@ RSpec.describe('Advanced Search') do
 
     # fill in data
     fill_in "First Name:", with: "Test"
+    click_button "Search"
+
+    # Expect User Bar to contain user's full name
+    expect(page).to have_content("Test User")
+  end
+
+  # Last Name search test
+  it 'displays the name of the logged in user, when given last name' do
+    # Sign in
+    user = FactoryBot.create(:user)
+    sign_in user
+
+    # Go to the search page
+    visit '/search'
+
+    # fill in data
+    fill_in "Last Name:", with: "User"
+    click_button "Search"
+
+    # Expect User Bar to contain user's full name
+    expect(page).to have_content("Test User")
+  end
+
+  it 'displays the name of the logged in user, when given class year' do
+    # Sign in
+    user = FactoryBot.create(:user)
+    sign_in user
+
+    # Go to the search page
+    visit '/search'
+
+    # fill in data
+    fill_in "Class Year:", with: 2024
+    click_button "Search"
+
+    # Expect User Bar to contain user's full name
+    expect(page).to have_content("Test User")
+  end
+
+  it 'displays the name of the logged in user, when given major' do
+    # Sign in
+    user = FactoryBot.create(:user)
+    sign_in user
+
+    # Go to the search page
+    visit '/search'
+
+    # fill in data
+    fill_in "Major:", with: "Computer Science"
+    click_button "Search"
+
+    # Expect User Bar to contain user's full name
+    expect(page).to have_content("Test User")
+  end
+
+  it 'displays the name of the logged in user, when given current city' do
+    # Sign in
+    user = FactoryBot.create(:user)
+    sign_in user
+
+    # Go to the search page
+    visit '/search'
+
+    # fill in data
+    fill_in "First Name:", with: "city"
     click_button "Search"
 
     # Expect User Bar to contain user's full name
