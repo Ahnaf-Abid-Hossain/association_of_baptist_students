24cc5c489c58012e86703c9c0b2fb6d705da99dc
Wes Taylor <jamsterwes@gmail.com>
2023-10-14 09:39:47 -0500

    reorganized Quick Links integration test, added pending tests for the update/edit endpoints

--- spec/system/quick_links_spec.rb
+++ spec/system/quick_links_spec.rb
@@ -9,95 +9,111 @@ RSpec.describe('Quick Links') do
     sign_in user
   end
 
-  it 'displays new links' do
-    # Create a link
-    next_order = (Link.maximum(:order) || 0) + 1
-    link = Link.create!(label: 'Test Link 77', url: 'testlink77.edu', order: next_order)
+  context 'viewing links' do
+    it 'displays new links' do
+      # Create a link
+      next_order = (Link.maximum(:order) || 0) + 1
+      link = Link.create!(label: 'Test Link 77', url: 'testlink77.edu', order: next_order)
+  
+      # Visit home page
+      visit '/'
+  
+      # Test for link
+      expect(page).to(have_link(link.label, href: link.url))
+    end
 
-    # Visit home page
-    visit '/'
-
-    # Test for link
-    expect(page).to(have_link(link.label, href: link.url))
+    pending 'displays links in order'
   end
 
-  it 'allows admins to try to make new links' do
-    # Promote ourself to admin
-    admin = FactoryBot.create(:admin_user)
-    sign_in admin
+  context 'making links' do
+    it 'allows admins to try to make new links' do
+      # Promote ourself to admin
+      admin = FactoryBot.create(:admin_user)
+      sign_in admin
 
-    # GET new link page
-    get '/links/new'
+      # GET new link page
+      get '/links/new'
 
-    # Expect to be allowed into the new page
-    expect(response).to(have_http_status(:ok))
-  end
+      # Expect to be allowed into the new page
+      expect(response).to(have_http_status(:ok))
+    end
 
-  it 'disallows non-admins from trying to make new links' do
-    # GET new link page
-    get '/links/new'
+    it 'prevents non-admins from trying to make new links' do
+      # GET new link page
+      get '/links/new'
 
-    # Expect to be directed away
-    expect(response).to(redirect_to('/'))
-  end
+      # Expect to be directed away
+      expect(response).to(redirect_to('/'))
+    end
 
-  it 'allows admins to create links' do
-    # Promote ourself to admin
-    admin = FactoryBot.create(:admin_user)
-    sign_in admin
+    it 'allows admins to create links' do
+      # Promote ourself to admin
+      admin = FactoryBot.create(:admin_user)
+      sign_in admin
 
-    # POST to links page
-    post '/links', params: {
-      link: {
-        label: 'Test',
-        url: 'https://test.com',
-        order: 800_000
+      # POST to links page
+      post '/links', params: {
+        link: {
+          label: 'Test',
+          url: 'https://test.com',
+          order: 800_000
+        }
       }
-    }
 
-    # Expect to be able to create link
-    expect(response).to(have_http_status(:found))
-  end
+      # Expect to be able to create link
+      expect(response).to(have_http_status(:found))
+    end
 
-  it 'disallows non-admins from creating links' do
-    # POST to links page
-    post '/links', params: {
-      link: {
-        label: 'Test',
-        url: 'https://test.com',
-        order: 800_000
+    it 'prevents non-admins from creating links' do
+      # POST to links page
+      post '/links', params: {
+        link: {
+          label: 'Test',
+          url: 'https://test.com',
+          order: 800_000
+        }
       }
-    }
 
-    # Expect to be forbidden
-    expect(response).to(have_http_status(:forbidden))
+      # Expect to be forbidden
+      expect(response).to(have_http_status(:forbidden))
+    end
   end
 
-  it 'allows admins to delete links' do
-    # Promote ourself to admin
-    admin = FactoryBot.create(:admin_user)
-    sign_in admin
+  context 'deleting links' do
+    it 'allows admins to delete links' do
+      # Promote ourself to admin
+      admin = FactoryBot.create(:admin_user)
+      sign_in admin
 
-    # Create link to delete
-    link = Link.create!(label: 'Test', url: 'https://test.com', order: 800_000)
+      # Create link to delete
+      link = Link.create!(label: 'Test', url: 'https://test.com', order: 800_000)
 
-    # DELETE to link
-    delete link_path(link)
+      # DELETE to link
+      delete link_path(link)
 
-    # Expect to be forbidden
-    expect(response).to(have_http_status(:found))
+      # Expect to be forbidden
+      expect(response).to(have_http_status(:found))
+    end
+
+    it 'prevents non-admins from deleting links' do
+      # Create link to delete
+      link = Link.create!(label: 'Test', url: 'https://test.com', order: 800_000)
+
+      # DELETE to link
+      delete link_path(link)
+
+      # Expect to be forbidden
+      expect(response).to(have_http_status(:forbidden))
+    end
   end
 
-  it 'disallows non-admins from deleting links' do
-    # Create link to delete
-    link = Link.create!(label: 'Test', url: 'https://test.com', order: 800_000)
+  context 'editing links' do
+    pending 'allows admins to try to edit links'
+    pending 'prevents non-admins from trying to edit links'
 
-    # DELETE to link
-    delete link_path(link)
-
-    # Expect to be forbidden
-    expect(response).to(have_http_status(:forbidden))
+    pending 'allows admins to actually edit links'
+    pending 'allows admins to actually edit links href'
+    pending 'allows admins to actually edit links title'
+    pending 'prevents non-admins from actually editing links'
   end
-
-  pending 'displays links in order'
 end
