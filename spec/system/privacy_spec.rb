d6f9730013b8cd9692342e444a5afc787f39d13d
Jacob Parker <parkerjacob37@gmail.com>
2023-10-15 12:53:10 +0000

    Changed a couple links to buttons, added a test. Will test the test now

---
+++ spec/system/privacy_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe('Advanced Search') do
+  before do
+    driven_by(:rack_test)
+  end
+
+  context 'when is_contact_email_private is False' do
+    it 'displays the user contact email' do
+      # create users
+      user = FactoryBot.create(:user)
+      user2 = FactoryBot.create(:user)
+
+      # Sign in as user2
+      sign_in user2
+
+      # Access the user's profile and check if the contact email is displayed
+      visit user_path(user)
+      expect(page).to have_content(user.contact_email)
+    end
+  end
+
+  #context 'when is_contact_email_private is True' do
+    #it 'does NOT display the user contact email' do
+      #user = FactoryBot.create(:user)
+      #user2 = FactoryBot.create(:user)
+
+      ## Sign in as user, and update is_contact_email_private to true
+      #sign_in user
+      #user.update(is_contact_email_private: true)
+
+      ## Sign out and sign in as user2
+      #sign_out user
+      #sign_in user2
+
+      # Access the user's profile and check if the contact email is NOT displayed
+      #visit user_path(user)
+      #expect(page).not_to have_content(user.contact_email)
+    #end
+  #end
+end
+
+end
