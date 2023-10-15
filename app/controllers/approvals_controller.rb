222a911215743bc7566eb4dea0d7c99a0ef60107
root <root@localhost>
2023-10-15 18:29:12 +0000

    approval hides/ meeting notes additions

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -1,6 +1,15 @@
 class ApprovalsController < ApplicationController
+  before_action :require_admin
   def index
     # @non_admin_alumni = Alumni.where.not(alum_status: 'admin')
     @non_admins = User.where(approval_status: 0)
   end
+
+  def require_admin
+    unless current_user && current_user.is_admin
+      flash[:alert] = "You do not have permission to access this page."
+      redirect_to root_path # or any other path you prefer
+    end
+  end
+  
 end
