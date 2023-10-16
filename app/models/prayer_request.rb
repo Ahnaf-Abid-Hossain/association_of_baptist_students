b0e8024ec296a018c8de477eb8a1f56d672cffa0
root <root@localhost>
2023-10-16 02:17:38 +0000

    Made some general test cases for prayer requests and one is failing

--- app/models/prayer_request.rb
+++ app/models/prayer_request.rb
@@ -1,3 +1,9 @@
 class PrayerRequest < ApplicationRecord
+  # Validations
+  validates :request, presence: true
+  validates :status, presence: true
+  validates :user_id, presence: true
+
+  # Associations
   belongs_to :user
 end
--- spec/factories/prayer_requests.rb
+++ spec/factories/prayer_requests.rb
@@ -2,29 +2,16 @@ FactoryBot.define do
   factory :prayer_request do
     request { 'Please pray for me.' }
     status { 'pending' }
+    association :user
 
-    # If user is not present, create a new transient user
-    transient do
-      user { FactoryBot.build(:user) }
-    end
-
-    after(:build) do |prayer_request, evaluator|
-      prayer_request.user = evaluator.user
-    end
-
-    # If you want to generate a valid prayer_request with specific attributes
-    factory :valid_prayer_request do
-      # Custom attributes
-      request { 'This is a specific prayer request.' }
-      status { 'not_read' }
-      # Add any other attributes you want to customize
-    end
-
-    # If you want to generate an invalid prayer_request (e.g., missing request)
-    factory :invalid_prayer_request do
+    factory :invalid_prayer_request_no_request do
       # Invalid because request is required
       request { nil }
-      status { 'pending' }
+    end
+
+    factory :invalid_prayer_request_no_status do
+      # Invalid because status is required
+      status { nil }
     end
   end
 end
