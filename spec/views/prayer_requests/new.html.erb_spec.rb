0b5565df190d42b8432faa4e88e76887a1247a79
Mario Morelos <mmorelos2@tamu.edu>
2023-10-16 19:08:17 -0500

    Separated tests in test cases for is_anonymous feature

--- spec/views/prayer_requests/new.html.erb_spec.rb
+++ spec/views/prayer_requests/new.html.erb_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe('prayer_requests/new') do
   end
 
   context 'admin creating their own prayer request' do
-    it 'renders the create prayer_request form' do
+    it 'renders the request field in the create prayer_request form' do
       sign_in @admin
       prayer_request = PrayerRequest.new
       assign(:prayer_request, prayer_request)
@@ -21,16 +21,40 @@ RSpec.describe('prayer_requests/new') do
 
       assert_select 'form[action=?][method=?]', prayer_requests_path, 'post' do
         assert_select 'input[name=?]', 'prayer_request[request]'
+      end
+    end
+  end
 
+  context 'admin creating their own prayer request' do
+    it 'does not render the status field in the create prayer_request form' do
+      sign_in @admin
+      prayer_request = PrayerRequest.new
+      assign(:prayer_request, prayer_request)
+
+      render
+
+      assert_select 'form[action=?][method=?]', prayer_requests_path, 'post' do
         assert_select 'input[name=?]', 'prayer_request[status]', count: 0
+      end
+    end
+  end
 
+  context 'admin creating their own prayer request' do
+    it 'renders the is_anonymous checkbox defaulted to anonymous in the create prayer_request form' do
+      sign_in @admin
+      prayer_request = PrayerRequest.new
+      assign(:prayer_request, prayer_request)
+
+      render
+
+      assert_select 'form[action=?][method=?]', prayer_requests_path, 'post' do
         assert_select 'input[type=?][checked=?][name=?]', 'checkbox', 'checked', 'prayer_request[is_anonymous]'
       end
     end
   end
 
   context 'user creating their own prayer request' do
-    it 'renders the create prayer_request form' do
+    it 'renders the request field in the create prayer_request form' do
       sign_in @user1
       prayer_request = PrayerRequest.new
       assign(:prayer_request, prayer_request)
@@ -39,9 +63,33 @@ RSpec.describe('prayer_requests/new') do
 
       assert_select 'form[action=?][method=?]', prayer_requests_path, 'post' do
         assert_select 'input[name=?]', 'prayer_request[request]'
+      end
+    end
+  end
 
+  context 'user creating their own prayer request' do
+    it 'does not render the status field in the create prayer_request form' do
+      sign_in @user1
+      prayer_request = PrayerRequest.new
+      assign(:prayer_request, prayer_request)
+
+      render
+
+      assert_select 'form[action=?][method=?]', prayer_requests_path, 'post' do
         assert_select 'input[name=?]', 'prayer_request[status]', count: 0
+      end
+    end
+  end
 
+  context 'user creating their own prayer request' do
+    it 'renders the is_anonymous checkbox defaulted to anonymous in the create prayer_request form' do
+      sign_in @user1
+      prayer_request = PrayerRequest.new
+      assign(:prayer_request, prayer_request)
+
+      render
+
+      assert_select 'form[action=?][method=?]', prayer_requests_path, 'post' do
         assert_select 'input[type=?][checked=?][name=?]', 'checkbox', 'checked', 'prayer_request[is_anonymous]'
       end
     end
