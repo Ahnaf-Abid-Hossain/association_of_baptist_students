10572aa2d3e86a450064268645d554c322fe22ee
Wesley Taylor <jamsterwes@gmail.com>
2023-10-26 01:10:43 -0500

    added author line in links page, corresponding tests

--- spec/views/links/show.html.erb_spec.rb
+++ spec/views/links/show.html.erb_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe('links/show') do
                     label: 'Label',
                     url: 'Url',
                     order: 2,
-                    user: FactoryBot.create(:admin_user)
+                    user: FactoryBot.create(:admin_user, user_first_name: "Show", user_last_name: "Test")
                   )
     )
   end
@@ -16,5 +16,6 @@ RSpec.describe('links/show') do
     expect(rendered).to(match(/Label/))
     expect(rendered).to(match(/Url/))
     expect(rendered).to(match(/2/))
+    expect(rendered).to(match(/Show Test/))
   end
 end
