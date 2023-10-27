cb5012bf1d1b78324ec93d7edc52c4c307182443
Wesley Taylor <jamsterwes@gmail.com>
2023-10-27 13:55:43 -0500

    fixed tests in views/links/show

--- spec/views/links/show.html.erb_spec.rb
+++ spec/views/links/show.html.erb_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 RSpec.describe('links/show') do
   before do
-    assign(:link, Link.create!(
-                    label: 'Label',
-                    url: 'Url',
-                    order: 2,
-                    user: FactoryBot.create(:admin_user, user_first_name: 'Show', user_last_name: 'Test')
-                  )
-    )
+    admin_user = FactoryBot.create(:admin_user, user_first_name: 'Show', user_last_name: 'Test')
+    assign(:link, FactoryBot.create(:link,
+      label: 'Label',
+      url: 'Url',
+      user: admin_user
+    ))
   end
 
   it 'renders attributes in <p>' do
     render
     expect(rendered).to(match(/Label/))
     expect(rendered).to(match(/Url/))
-    expect(rendered).to(match(/2/))
     expect(rendered).to(match(/Show Test/))
   end
 end
