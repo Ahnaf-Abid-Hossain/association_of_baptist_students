892ab4bd99ac049926b5356866e4775649e292f6
root <root@localhost>
2023-11-06 07:58:16 +0000

    approval toggler, css addons, meeting notes css and basic search feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,29 @@ class UsersController < ApplicationController
 
   def decline
     @alumni = User.find(params[:id])
-    @alumni.update!(approval_status: -1)
-    redirect_to(users_path, notice: 'Alumni declined successfully.')
+    if @alumni != current_user
+      @alumni.update!(approval_status: -1)
+      redirect_to(users_path, notice: 'Alumni declined successfully.')
+    else
+      redirect_to(users_path, alert: "You can't decline yourself.")
+    end
   end
 
   def approve_admin
     @alumni = User.find(params[:id])
-    @alumni.update(is_admin: 1)
-    redirect_to(users_path, notice: 'Alumni made Admin successfully')
+    if @alumni != current_user
+      if @alumni.is_admin?
+        @alumni.update(is_admin: 0)
+        notice_message = 'Alumni admin status revoked successfully.'
+      else
+        @alumni.update(is_admin: 1)
+        notice_message = 'Alumni made admin successfully.'
+      end
+    else
+      notice_message = "You can't modify your own admin status."
+    end
+  
+    redirect_to(users_path, notice: notice_message)
   end
 
   # GET /users or /users.json
